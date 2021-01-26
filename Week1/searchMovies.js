cb9339b1aaad14b3229d764cd9513877030f4f5c
Lipi <lipi17dpatnaik@gmail.com>
2021-01-26 12:36:00 +0530

    Add week1 files

---
+++ Week1/searchMovies.js
@@ -0,0 +1,14 @@
+function search_movie() { 
+    let input = document.getElementById('searchbar').value 
+    input=input.toLowerCase(); 
+    let x = document.getElementsByClassName('videoBoxes'); 
+    
+    for (i = 0; i < x.length; i++) {  
+        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+            x[i].style.display="none"; 
+        } 
+        else { 
+            //x[i].style.display="list-item";                  
+        } 
+    } 
+}
