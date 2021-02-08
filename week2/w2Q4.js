86b12313fcb3245bbd7053d9cbd3f3005ffa3b66
Lipi Deepaakshi Patnaik <40713338+lipi17dpatnaik@users.noreply.github.com>
2021-02-08 15:08:20 +0530

    Update w2Q4.js

--- week2/w2Q4.js
+++ week2/w2Q4.js
@@ -11,7 +11,28 @@ Array.prototype.newForEach=function(func){
   func(this[index],index,this);
   }
 }
-Function.prototype.newBind= function(inArgs){
+
+
+Function.prototype.newBind = function(...args){
+  let currentFunction = this;
+  let bindingObject = args[0];
+  let params = args.slice(1);
+  return function(...localArgs){
+    let functionArgs = params.concat(localArgs);
+    currentFunction.apply(bindingObject,functionArgs); 
+  }
+}
+
+let fn = function(object){
+  console.log(this + object);
+}
+
+let object = "Hello";
+let bindCheck = fn.newBind(object);
+bindCheck();
+bindCheck(" World");
+
+/*Function.prototype.newBind= function(inArgs){
   const args=Array.from(arguments).slice(1);
   const self=this;
   return function(){
@@ -27,7 +48,7 @@ console.log(add.newBind(null,5,10)());
 const sampleValues=[1,2,3];
 const mapOutputValues=sampleValues.newMap(function(value,index,array){
   return value*5;
-})
+})*/
 
 console.log(mapOutputValues);
 sampleValues.newForEach(function(value,index,array){
