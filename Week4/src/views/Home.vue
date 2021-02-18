4b43df646730c137ef49e83de0ee9f780899ef39
Lipi Deepaakshi Patnaik <40713338+lipi17dpatnaik@users.noreply.github.com>
2021-02-18 12:30:06 +0530

    Update Home.vue

--- Week4/src/views/Home.vue
+++ Week4/src/views/Home.vue
@@ -9,18 +9,18 @@
         <router-link to="/create">Create Product</router-link>
       </div>
     </div>
-    <HelloWorld v-for="(item,index) in items" :key="item.bin" :msg="item.name" :prodNo="item.id" :binNo="item.bin" :cardType="item.cardNetwork" :cardURL="getLogoURL(item.cardNetwork)" :bgColor="getLogoBgColor(item.cardNetwork)" :itemIndex="index" />
+    <Cards v-for="(item,index) in items" :key="item.bin" :msg="item.name" :prodNo="item.id" :binNo="item.bin" :cardType="item.cardNetwork" :cardURL="getLogoURL(item.cardNetwork)" :bgColor="getLogoBgColor(item.cardNetwork)" :itemIndex="index" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Cards from "@/components/Cards.vue"; // @ is an alias to /src
 import data from "@/assets/cards.json";
 
 @Component({
   components: {
-    HelloWorld
+    Cards
   }
 })
 
@@ -52,7 +52,7 @@ export default class Home extends Vue {
 </script>
 
 <style>
-.hello {
+.cardsContainer {
   display:inline-block;
 }
 </style>
